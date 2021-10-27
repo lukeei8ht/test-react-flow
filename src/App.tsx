8c17c8981b0131983571fbe2672e373c0989a97b
a-hisatomi <a_hisatomi@dreamarts.co.jp>
2021-10-27 16:46:27 +0900

    feat: sample program

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,103 @@
 import "./App.css";
 
-import React from "react";
-import ReactFlow from "react-flow-renderer";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import ReactFlow, {
+  ArrowHeadType,
+  Background,
+  Connection,
+  Controls,
+  Edge,
+  Elements,
+  MiniMap,
+  OnLoadParams,
+  addEdge,
+  removeElements,
+  updateEdge,
+  useStoreState,
+} from "react-flow-renderer";
 
-const elements = [
-  {
-    id: "1",
-    type: "input", // input node
-    data: { label: "Input Node" },
-    position: { x: 250, y: 25 },
-  },
-  // default node
-  {
-    id: "2",
-    // you can also pass a React component as a label
-    data: { label: <div>Default Node</div> },
-    position: { x: 100, y: 125 },
-  },
-  {
-    id: "3",
-    type: "output", // output node
-    data: { label: "Output Node" },
-    position: { x: 250, y: 250 },
-  },
-  // animated edge
-  { id: "e1-2", source: "1", target: "2", animated: true },
-  { id: "e2-3", source: "2", target: "3" },
-];
+const App = () => {
+  const [elements, setElements] = useState<Elements>([]);
 
-function App() {
+  const onLoad = (reactFlowInstance: OnLoadParams) => {
+    reactFlowInstance.fitView();
+  };
+
+  const onElementsRemove = (elementsToRemove: Elements) => {
+    setElements((els) => removeElements(elementsToRemove, els));
+  };
+
+  const onConnect = (params: Edge | Connection) => {
+    console.log(params);
+    setElements((els) =>
+      addEdge(
+        {
+          ...params,
+          // 矢印のついたエッジにする
+          arrowHeadType: ArrowHeadType.ArrowClosed,
+        },
+        els,
+      ),
+    );
+  };
+
+  const onEdgeUpdate = (oldEdge: Edge, newConnection: Connection) => {
+    setElements((els) => updateEdge(oldEdge, newConnection, els));
+  };
+
   return (
     <div style={{ height: "100vh" }}>
-      <ReactFlow elements={elements} />
+      <ReactFlow
+        elements={elements}
+        onLoad={onLoad}
+        onElementsRemove={onElementsRemove}
+        onConnect={onConnect}
+        onEdgeUpdate={onEdgeUpdate}
+      >
+        <Background />
+        <Controls />
+        <MiniMap />
+        <MyDebugger setElements={setElements} />
+      </ReactFlow>
     </div>
   );
-}
+};
+
+const MyDebugger = ({
+  setElements,
+}: {
+  setElements: React.Dispatch<React.SetStateAction<Elements>>;
+}) => {
+  const nodes = useStoreState((state) => state.nodes);
+  const edges = useStoreState((state) => state.edges);
+  console.log("nodes", nodes);
+  console.log("edges", edges);
+
+  const [id, setId] = useState(0);
+  const transform = useStoreState((state) => state.transform);
+
+  const createNewNode = () => {
+    setId((id) => id + 1);
+    const idStr = String(id);
+
+    setElements((els) => [
+      ...els,
+      {
+        id: idStr,
+        data: { label: `Node ${idStr}` },
+        position: {
+          x: -transform[0] / transform[2] + 100,
+          y: -transform[1] / transform[2] + 100,
+        },
+      },
+    ]);
+  };
+
+  return (
+    <button style={{ position: "fixed", zIndex: 100 }} onClick={createNewNode}>
+      ノードを追加
+    </button>
+  );
+};
 
 export default App;
